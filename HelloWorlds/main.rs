c1986251091b461fc7ae587c60cebe083825d2f9
Jan Traußnigg <trajan20@htl-kaindorf.at>
2023-08-11 12:27:02 +0200

    fn eingebunden und modifiziert

--- HelloWorlds/main.rs
+++ HelloWorlds/main.rs
@@ -3,10 +3,10 @@ fn main() {
     println!("I'm a Rustacean!");
     let x = 5 + /* 90 + */ 5;
     println!("Is `x` 10 or 100? x = {}", x);
-    /*fn -> i32 { foo } // foo is a parameter, i32 is the return type
+    fn foo(x: i32) -> i32 { x*2 }
     let y: i32 = 5;
     let f: fn(i32) -> i32 = foo;
     println!("f(y) = {}", f(y));
     println!("f(10) = {}", f(10));
-    println!("f(20) = {}", f(20)); */
+    println!("f(20) = {}", f(20)); 
 }
