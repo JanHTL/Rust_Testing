12c90c47f4ef638f011e1414f46c83849d1d9e0a
Jan Traußnigg <trajan20@htl-kaindorf.at>
2023-08-11 12:22:04 +0200

    Test eines neuen Branches und Test von let

--- HelloWorlds/main.rs
+++ HelloWorlds/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    println!("I'm a Rustacean!");
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+    /*fn -> i32 { foo } // foo is a parameter, i32 is the return type
+    let y: i32 = 5;
+    let f: fn(i32) -> i32 = foo;
+    println!("f(y) = {}", f(y));
+    println!("f(10) = {}", f(10));
+    println!("f(20) = {}", f(20)); */
 }
